0f70b45453b5fe9ca5df45f4b6b4232bea8c1570
benclark1110 <benclark1110@gmail.com>
2019-04-02 00:52:53 -0400

    adding updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,39 @@
 timeRemaining = 5;
 questionCount = 0;
+correctAnswers = 0;
+incorrectAnswers = 0;
 
 questionGuessesArray = [
           {
             question: "This is Question 1",
-            answer1: "This is Q1A1",
-            answer2: "This is Q1A2",
-            answer3: "This is Q1A3",
-            answer4: "This is Q1A4"
+            answerChoices: ["This is Q1A1", "This is Q1A2", "This is Q1A3", "This is Q1A4"],
+            answer: 0
             } , {
             question: "This is Question 1",
-            answer1: "This is Q1A1",
-            answer2: "This is Q1A2",
-            answer3: "This is Q1A3",
-            answer4: "This is Q1A4"
+            answerChoices: ["This is Q2A1", "This is Q2A2", "This is Q2A3", "This is Q2A4"],
+            answer: 1
             } , {
             question: "This is Question 1",
-            answer1: "This is Q1A1",
-            answer2: "This is Q1A2",
-            answer3: "This is Q1A3",
-            answer4: "This is Q1A4"
+            answerChoices: ["This is Q3A1", "This is Q3A2", "This is Q3A3", "This is Q3A4"],
+            answer: 2
             } 
 ]
 
 $(document).ready(function(){
 
     console.log(questionGuessesArray[2].answer1);//can reach in and grab the specific answer i want
+
+    function runTime(){
+        setIntervalID = setInterval(function(){
+            $("#timeRemaining").html("<h2> Time Remaining: " + timeRemaining + " seconds </h2>");
+            if (timeRemaining === 0) {
+                clearInterval(setIntervalID);
+                questionCount ++;
+                console.log(questionCount);//not affectig anything yet
+            };
+            timeRemaining --;
+        },1000 );
+    }
 
     //when start button is selected
     $("#start").on("click", function(){
@@ -34,30 +42,55 @@ $(document).ready(function(){
         $("#start").hide();
 
         
-        function runTime(){
-            var setIntervalID = setInterval(function(){
-                $("#timeRemaining").html("<h2>" + timeRemaining + "</h2>");
-                timeRemaining --;
-                if (timeRemaining === 0) {
-                    clearInterval(setIntervalID);
-                    questionCount ++;
-                    console.log(questionCount);//not affectig anything yet
-                };
-            },1000 );
+        
+
+                $("#question").html(questionGuessesArray[0].question);
+                $("#answer1").html(questionGuessesArray[0].answerChoices[0]);
+                $("#answer2").html(questionGuessesArray[0].answerChoices[1]);
+                $("#answer3").html(questionGuessesArray[0].answerChoices[2]);
+                $("#answer4").html(questionGuessesArray[0].answerChoices[3]);
+ 
+        runTime();
+
+    });
+
+    $(".answers").on("click", function(){
+        clearInterval(setIntervalID);
+        //identifying which answer was clicked
+        if (questionGuessesArray[0] && this.id == "answer1") {
+            console.log("right answer");
+            setTimeout (function(){ 
+                correctAnswers ++;
+                console.log(correctAnswers);
+                $(".question").empty();
+                $(".answers").empty();
+                $("#question").html(questionGuessesArray[1].question);
+                $("#answer1").html(questionGuessesArray[1].answerChoices[0]);
+                $("#answer2").html(questionGuessesArray[1].answerChoices[1]);
+                $("#answer3").html(questionGuessesArray[1].answerChoices[2]);
+                $("#answer4").html(questionGuessesArray[1].answerChoices[3]);
+                clearInterval(setIntervalID);
+                timeRemaining = 5;
+                runTime();
+            }, 2000)
+        } else {
+            console.log("wrong answer");
+            setTimeout (function(){ 
+                incorrectAnswers ++;
+                console.log(incorrectAnswers);
+                $(".question").empty();
+                $(".answers").empty();
+                $("#question").html(questionGuessesArray[1].question);
+                $("#answer1").html(questionGuessesArray[1].answerChoices[0]);
+                $("#answer2").html(questionGuessesArray[1].answerChoices[0]);
+                $("#answer3").html(questionGuessesArray[1].answerChoices[0]);
+                $("#answer4").html(questionGuessesArray[1].answerChoices[0]);
+                clearInterval(setIntervalID);
+                timeRemaining = 5;
+                runTime();
+            }, 2000)
         }
 
-        //function showQuestionAndAnswers(){
-          //  for (i = 0; i < questionGuessesArray; i++) {
-                $("#question").html(questionGuessesArray[0].question);
-                $("#answer1").html(questionGuessesArray[0].answer1);
-                $("#answer2").html(questionGuessesArray[0].answer2);
-                $("#answer3").html(questionGuessesArray[0].answer3);
-                $("#answer4").html(questionGuessesArray[0].answer4);
-            //}
-        //}
-
-        //showQuestionAndAnswers()
-        runTime();
 
     });
 });
